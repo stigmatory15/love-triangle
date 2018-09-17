3b9bb57e9440742bd481c12aedf87b3159f02745
stigmatory15 <repka_dmitriy@mail.ru>
2018-09-17 17:46:18 +0300

    2 task complete

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    var sum=0;
+for(var i=0; i<preferences.length;i++){
+    var x=preferences[i];
+    var y=preferences[x-1];
+    if(preferences[y-1]===(i+1) && x!=y){
+        sum++;
+        delete preferences[i];
+    }
+    }
+    return sum;
 };
